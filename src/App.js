90a47995ad5fa706eb61d5a3c276762eb7da2e6e
krystek77 <krystian.wrona77@gmail.com>
2020-06-30 09:59:56 +0200

    Update App.js
    I have added routes to another pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navigation from "./Navigation";
 import Welcome from "./Welcome";
+import Register from "./Register";
+import Login from "./Login";
+import Meetings from "./Meetings";
 import Home from "./Home";
 import "./App.css";
 
@@ -13,7 +16,14 @@ function App() {
       <div className="App">
         <Navigation user={user} />
         <Welcome user={user} />
-        <Route path="/" render={(props) => <Home {...props} user={user} />} />
+        <Route path="/register" render={(props) => <Register {...props} />} />
+        <Route path="/login" render={(props) => <Login {...props} />} />
+        <Route path="/meetings" render={(props) => <Meetings {...props} />} />
+        <Route
+          path="/"
+          exact
+          render={(props) => <Home {...props} user={user} />}
+        />
       </div>
     </Router>
   );
