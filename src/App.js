02fd5207eb22745204abe9d3a08d2d750d0b1713
krystek77 <krystian.wrona77@gmail.com>
2020-06-30 09:14:46 +0200

    extract the home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Navigation from "./Navigation";
 import Welcome from "./Welcome";
+import Home from "./Home";
 import "./App.css";
 
 function App() {
@@ -13,32 +14,7 @@ function App() {
       {/** Welcome */}
       <Welcome user={user} />
       {/** Home */}
-      <div className="container">
-        <div className="row">
-          <h1 className="col text-center">Meeting log</h1>
-        </div>
-        <div className="row">
-          <div className="col-md-8 col-lg-6 mx-auto">
-            <p className="text-center font-weight-light">
-              The simple application creates meetings, allows people to check in
-              and pick random users to award giveaways. It is Single Page
-              Application which includes connection to a database and routing.
-            </p>
-          </div>
-        </div>
-        {!user && (
-          <div className="row">
-            <div className="col text-center">
-              <button type="button" className="btn btn-success mr-2 btn-lg">
-                register
-              </button>
-              <button type="button" className="btn btn-success mr-2 btn-lg">
-                log in
-              </button>
-            </div>
-          </div>
-        )}
-      </div>
+      <Home user={user} />
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export default function Home(props) {
+  return (
+    <div className="container">
+      <div className="row">
+        <h1 className="col text-center">Meeting log</h1>
+      </div>
+      <div className="row">
+        <div className="col-md-8 col-lg-6 mx-auto">
+          <p className="text-center font-weight-light">
+            The simple application creates meetings, allows people to check in
+            and pick random users to award giveaways. It is Single Page
+            Application which includes connection to a database and routing.
+          </p>
+        </div>
+      </div>
+      {!props.user && (
+        <div className="row">
+          <div className="col text-center">
+            <button type="button" className="btn btn-success mr-2 btn-lg">
+              register
+            </button>
+            <button type="button" className="btn btn-success mr-2 btn-lg">
+              log in
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
