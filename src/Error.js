18f864eec839f89c032994ec0bf41001c62a78fb
krystek77 <krystian.wrona77@gmail.com>
2020-06-30 21:40:17 +0200

    Update Error.js
    I have improved error component

--- src/Error.js
+++ src/Error.js
@@ -2,7 +2,12 @@ import React from "react";
 
 export default function Error(props) {
   return (
-    <div className="bg-danger text-light px-2 py-1 mt-1 text-lowercase rounded">
+    <div className="bg-danger text-light px-2 py-1 mt-1 text-lowercase rounded text-center">
+      {props.code && (
+        <h2 className="text-center">
+          Error: <span className="text-uppercase text-warning">{props.code}</span>
+        </h2>
+      )}
       {props.message}
     </div>
   );
