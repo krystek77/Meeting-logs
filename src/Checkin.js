db9ed5e9628a4bafe4a9fafcbcdf34c4a9d45eda
krystek77 <krystian.wrona77@gmail.com>
2020-07-01 14:05:51 +0200

    Update MeetingsList.js
    Added the condition to render meetings list if meetings exist and there
    are more of then zero.

---
+++ src/Checkin.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Checkin() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="text-center text-uppercase font-weight-bold">
+            Checkin
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/MeetingsList.js
+++ src/MeetingsList.js
@@ -3,18 +3,22 @@ import Meeting from "./Meeting";
 
 export default function MeetingsList(props) {
   return (
-    <div className="row justify-content-center">
-      <div className="col-lg-6">
-        {props.meetings.map((meeting) => {
-          return (
-            <Meeting
-              key={meeting.id}
-              meeting={meeting}
-              deleteMeeting={() => props.deleteMeeting(meeting.id)}
-            />
-          );
-        })}
-      </div>
-    </div>
+    <React.Fragment>
+      {props.meetings && props.meetings.length && (
+        <div className="row justify-content-center">
+          <div className="col-lg-6">
+            {props.meetings.map((meeting) => {
+              return (
+                <Meeting
+                  key={meeting.id}
+                  meeting={meeting}
+                  deleteMeeting={() => props.deleteMeeting(meeting.id)}
+                />
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </React.Fragment>
   );
 }
