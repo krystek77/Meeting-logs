85f505e487c4a783781bf6bb946d02fa73b11e83
krystek77 <krystian.wrona77@gmail.com>
2020-07-01 13:15:10 +0200

    Update Meeting.js
    Changed look of subtitle and description

--- src/Meeting.js
+++ src/Meeting.js
@@ -5,10 +5,10 @@ export default function Meeting(props) {
     <div className="card mt-3">
       <div className="card-body">
         <h2 className="card-title mb-0">{props.meeting.meetingName}</h2>
-        <span class="card-subtitle mb-2 text-muted">
+        <span className="card-subtitle mb-2 text-muted">
           added: <strong>{props.meeting.added.slice(0, 10)}</strong>
         </span>
-        <p class="card-text text-truncate">{props.meeting.description}</p>
+        <p className="card-text text-truncate">{props.meeting.description}</p>
       </div>
     </div>
   );
