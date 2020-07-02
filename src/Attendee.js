7e7089eb7c85fc9c71a85cf521abad4dd0d59666
krystek77 <krystian.wrona77@gmail.com>
2020-07-02 07:40:02 +0200

    Update Attendee.js
    Added control buttons to delete the attendee, send mail and add the star

--- src/Attendee.js
+++ src/Attendee.js
@@ -1,11 +1,45 @@
 import React from "react";
+import { AiFillStar, AiFillDelete } from "react-icons/ai";
+import { MdEmail } from "react-icons/md";
 
 export default function Attendee(props) {
   const { attendeeName, attendeeEmail } = props;
   return (
-    <li className="list-group-item">
-      <h3>{attendeeName}</h3>
-      <span>{attendeeEmail}</span>
+    <li className="list-group-item d-flex align-items-center">
+      <div
+        className="btn-group"
+        role="control-buttons"
+        aria-label="control-buttons"
+      >
+        <button
+          type="button"
+          className="btn btn-outline-secondary"
+          title="Add to favorite"
+          onClick={() => console.log("Set star")}
+        >
+          <AiFillStar />
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-danger"
+          title="Delete attendee"
+          onClick={() => console.log("Delete attendee")}
+        >
+          <AiFillDelete />
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-info"
+          title="Delete attendee"
+          onClick={() => console.log("Delete attendee")}
+        >
+          <MdEmail />
+        </button>
+      </div>
+      <div className="ml-3">
+        <h3>{attendeeName}</h3>
+        <span>{attendeeEmail}</span>
+      </div>
     </li>
   );
 }
