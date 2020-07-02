21e0c8ac53ed16ba40213b8a507cded47139df5b
krystek77 <krystian.wrona77@gmail.com>
2020-07-02 08:32:44 +0200

    Update Attendee.js
    Removed role and aria-label

--- src/Attendee.js
+++ src/Attendee.js
@@ -3,41 +3,41 @@ import { AiFillStar, AiFillDelete } from "react-icons/ai";
 import { MdEmail } from "react-icons/md";
 
 export default function Attendee(props) {
-  const { attendeeName, attendeeEmail } = props;
+  const { attendeeName, attendeeEmail, adminUser, userID } = props;
+  const admin = adminUser === userID ? true : false;
   return (
     <li className="list-group-item d-flex align-items-center">
-      <div
-        className="btn-group"
-        role="control-buttons"
-        aria-label="control-buttons"
-      >
-        <button
-          type="button"
-          className="btn btn-outline-secondary"
-          title="Add to favorite"
-          onClick={() => console.log("Set star")}
-        >
-          <AiFillStar />
-        </button>
-        <button
-          type="button"
-          className="btn btn-outline-danger"
-          title="Delete attendee"
-          onClick={() => console.log("Delete attendee")}
-        >
-          <AiFillDelete />
-        </button>
-        <button
-          type="button"
-          className="btn btn-outline-info"
-          title="Delete attendee"
-          onClick={() => console.log("Delete attendee")}
-        >
-          <MdEmail />
-        </button>
-      </div>
-      <div className="ml-3">
-        <h3>{attendeeName}</h3>
+      {admin && (
+        <div className="btn-group">
+          <button
+            type="button"
+            className="btn btn-outline-secondary"
+            title="Add to favorite"
+            onClick={() => console.log("Set star")}
+          >
+            <AiFillStar />
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-danger"
+            title="Delete attendee"
+            onClick={() => console.log("Delete attendee")}
+          >
+            <AiFillDelete />
+          </button>
+          <button
+            type="button"
+            className="btn btn-outline-info"
+            title="Delete attendee"
+            onClick={() => console.log("Delete attendee")}
+          >
+            <MdEmail />
+          </button>
+        </div>
+      )}
+
+      <div className={admin ? "ml-3" : "mx-auto"}>
+        <h3 className={admin ? "" : "text-center"}>{attendeeName}</h3>
         <span>{attendeeEmail}</span>
       </div>
     </li>
